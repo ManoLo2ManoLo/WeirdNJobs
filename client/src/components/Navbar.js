5fd41ae4e5df6cfcfc2da9e0794e34a40a3f17f7
NeXFP <giovannycardoso50@gmail.com>
2022-01-31 01:23:10 -0500

    Worked on adding LoginForm, Navbar and Signup Form

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Navbar } from 'react-bulma-components';
+import logo from '../assets/images/WeirdNJobs.png';
+
+function Navbar() {
+    return(
+        <Navbar>
+            <NavbarBrand>
+                <NavbarItem href='#'>
+                    <img src={logo} alt='Logo for site' height={40} width={120}/>
+                </NavbarItem>
+                <NavbarBurger />
+            </NavbarBrand>
+            <NavbarMenu>
+                <NavbarContainer>
+                    <NavbarItem href='#Home'>
+                        <NavbarLink>
+                            Home
+                        </NavbarLink>
+                    </NavbarItem>
+                </NavbarContainer>
+                <NavbarContainer align='end'>
+                    <NavbarItem href='#LogIn'>
+                        Log In
+                    </NavbarItem>
+                </NavbarContainer>
+                <NavbarDivider />
+                <NavbarContainer align='end'>
+                    <NavbarItem href='#LogIn'>
+                        Sign Up
+                    </NavbarItem>
+                </NavbarContainer>
+            </NavbarMenu>
+        </Navbar>
+    )
+};
+
+export default Navbar;
