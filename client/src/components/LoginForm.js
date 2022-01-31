8c76d845b684d501dd0a2df66d574a5624089a1f
NeXFP <giovannycardoso50@gmail.com>
2022-01-31 00:33:13 -0500

    Created SignUp form and installed bulma

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Form } from 'react-bulma-components';
+import { Button, ButtonGroup } from 'react-bulma-components';
+import { Modal } from 'react-bulma-components';
+
+function signUpForm() {
+    const { Input, Field, Control, Label } = Form;
+    const { ModalCard, ModalCardHead, ModalCardTitle, ModalCardBody, ModalCardFoot} = Modal;
+    return (
+        <>
+            <ButtonGroup renderAs={function noRefCheck() { }}>
+                <Button
+                    color="info"
+                    onClick={function noRefCheck() { }}
+                >
+                    Login
+                </Button>
+            </ButtonGroup>
+            <Modal
+                onClose={function noRefCheck() { }}
+            >
+                <ModalCard>
+                    <ModalCardHead>
+                        <ModalCardTitle>
+                            Login Form
+                        </ModalCardTitle>
+                    </ModalCardHead>
+                    <ModalCardBody>
+                        <>
+                        <Field>
+                                <Label>
+                                    Email
+                                </Label>
+                                <Control>
+                                    <Input
+                                        placeholder="Email goes here!"
+                                        type="email"
+                                    />
+                                </Control>
+                            </Field>
+                            <Field>
+                                <Label>
+                                    Password
+                                </Label>
+                                <Control>
+                                    <Input
+                                        placeholder="Write here weirdo!"
+                                        type="password"
+                                    />
+                                </Control>
+                            </Field>
+                        </>
+                    </ModalCardBody>
+                    <ModalCardFoot
+                        align="center"
+                        hasAddons
+                        renderAs={function noRefCheck() { }}
+                    >
+                        <Button color="success">
+                            Sign-Up
+                        </Button>
+                    </ModalCardFoot>
+                </ModalCard>
+            </Modal>
+        </>
+
+    )
+};
+
+module.exports = signUpForm;
