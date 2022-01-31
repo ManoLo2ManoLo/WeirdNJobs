16c7e6b17a1944501d80df7ca523d0d90aec846e
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2022-01-31 02:02:58 -0500

    edited queries updated util

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import {gql} from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gq1`
+    mutation addUser($firstName: String!, $lastName: String!, $username: String!, $email: String!, $password: String!, $county: String!) {
+        addUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password, county: $county) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`
+
+export const UPDATE_USER = gql`
+    mutation updateUser($firstName: String!, $lastName: String!, $username: String!, $email: String!, $password: String!, $county: String!, $profileImage: String!, $description: String!) {
+        updateUser(firstName: $firstName, lastName: $lastName, username: $username, email: $email, password: $password, county: $county, profileImage: $profileImage, description: $description) {
+            _id
+        }
+    }
+`
+export const ADD_SERVICE = gql`
+    mutation addService($serviceTitle: String!, $serviceBody: String!, $fee: Int!) {
+        addService(serviceTitle: $serviceTitle, serviceBody: $serviceBody, fee: $fee) {
+            _id
+        }
+    }
+`
+
+export const ADD_USER_REVIEW = gql`
+    mutation addUserReview($userId: ID!, $reviewTitle: String!, $reviewBody: String!, $rating: Int!) {
+        addUserReview(userId: $userId, reviewTitle: $reviewTitle, reviewBody: $reviewBody, rating: $rating) {
+            _id
+        }
+    }
+`
+
+export const ADD_SERVICE_REVIEW = gql`
+    mutation addServiceReview($serviceId: ID!, $reviewTitle: String!, $reviewBody: String!, $rating: Int!) {
+        addServiceReview(serviceId: $serviceId, reviewTitle: $reviewTitle, reviewBody: $reviewBody, rating: $rating) {
+            _id
+        }
+    }
+`
+
+export const ADD_ORDER = gql`
+    mutation addOrder($services: [ID]!) {
+        addOrder(services: $services) {
+            _id
+            purchaseDate
+            services {
+                _id
+            }
+        }
+    }
+`
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,20 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ id: context.user._id })
+                    .select('-__v -password')
+                    .populate('services')
+                    .populate('orders')
+                    .populate('reviews')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+
         counties: async () => {
             return await County.find();
         },
