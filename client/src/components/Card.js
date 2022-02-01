ee87b58835d5fa3e8ba54ab0dc07c0ed40a2af09
ManoLo2ManoLo <manuel_canas@aol.com>
2022-02-01 08:00:42 -0500

    re udjusted nav and styling classes

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
 function Card() {
-    const inputStyle = {
-        width : '30%'
-    };
-
     return (
         <div className="card">
             <header className="card-header">
-                <p className="card-header-title" style={inputStyle}>
-                  <a href="/" style={inputStyle}>@noloC</a>  Handyman Work for light switches
+                <p className="card-header-title width30">
+                  <a href="/" className='width30'>@nexusC</a>  Handyman Work for uselessness
                 </p>
                 <button className="card-header-icon" aria-label="more options">
                     <span className="icon">
@@ -19,23 +15,20 @@ function Card() {
             </header>
             <div className="card-content">
                 <div className="content">
-                    Hello my name is NoloC. I'm an experienced electrician with a focus on light switches. 
-                        The work I'm looking for currently is one where I come to your home, inspect the lights, 
-                        once the lights are inspected, I make myself comfortable, take your alcohol and drink
-                        until I get paid my money.
+                    Hello my name is NexusC. I'm an experienced plumber with the a brain of a squirrel.
+                    The work I'm looking for currently is one where I come to your home, do nothing,
+                    once I get nothing done, I make myself comfortable, take your vape and vape
+                    until I get paid my money.
 
-                        *** Alcohol Preference - Titos
-                        *** Charge is 250$ per light switch and 10$ per cup of alcohol.
-
-        
-                    <a href="/" style={inputStyle}>@noloC</a>. <a href="/" style={inputStyle}>#weirdwork</a> <a href="/" style={inputStyle}>#drunk</a>
-                    <br></br>
-                        <time datetime="2022-1-30" style={inputStyle}>11:09 PM - 30 Jan 2022</time>
+                    *** Vape Preference - Nut
+                    *** Charge is 250$ per nothing and 10$ per hit of vape.
+                    <a href="/" className='width30'>@nexusC</a>. <a href="/" className='width30'>#weirdwork</a> <a href="/" className='width30'>#vapingLyfe</a>
+                    <time datetime="2022-1-30" className='width30'>11:09 PM - 30 Jan 2022</time>
                 </div>
             </div>
             <footer className="card-footer">
-                <a href="#" className="card-footer-item" style={inputStyle}>Save</a>
-                <a href="#" className="card-footer-item" style={inputStyle}>Purchase</a>
+                <a href="#" className="card-footer-item width30">Save</a>
+                <a href="#" className="card-footer-item width30">Purchase</a>
             </footer>
         </div>
     );
