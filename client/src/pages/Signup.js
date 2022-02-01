f87aafeebf78c683eaf4e97c457a5a11825d13a6
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2022-02-01 03:31:22 -0500

    connected login and sign up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Auth from '../utils/auth';
 
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations'
 
 function Signup() {
+  const [formState, setFormState] = useState({ firstName: '', lastName: '', username: '', email: '', password: '', verify: '', county: '' });
+  const [addUser] = useMutation(ADD_USER);
+
+  const handleFormSubmit = async (event) => {
+    if (formState.firstName && formState.lastName && formState.username && formState.email && 
+    formState.password && formState.verify && formState.county && formState.password === formState.verify) {
+      event.preventDefault();
+
+      const mutationResponse = await addUser({
+        variables: {
+          email: formState.email,
+          password: formState.password,
+          firstName: formState.firstName,
+          lastName: formState.lastName,
+          username: formState.username,
+          county: formState.county
+        }
+      });
+
+      const token = mutationResponse.data.addUser.token;
+      Auth.login(token);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
   return (
-    <>
+    <form onSubmit={handleFormSubmit}>
       <div className='field'>
         <label className='label' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>First Name</label>
         <div className='control' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='text' ></input>
+          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='text' onChange={handleChange}></input>
         </div>
       </div>
       <div className='field'>
         <label className='label' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Last Name</label>
         <div className='control' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='text' ></input>
+          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='text' onChange={handleChange}></input>
         </div>
       </div>
       <div className='field'>
         <label className='label' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Username</label>
         <div className='control' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='text' ></input>
+          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='text' onChange={handleChange}></input>
         </div>
       </div>
       <div className='field'>
         <label className='label' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Email</label>
         <div className='control' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='email' ></input>
+          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='email' onChange={handleChange}></input>
         </div>
         <p className='help is-danger' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>This email is invalid!</p>
       </div>
@@ -33,13 +68,13 @@ function Signup() {
       <div className="field">
         <label className="label" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Password</label>
         <div className="control" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <input className="input is-small" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type="text"></input>
+          <input className="input is-small" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type="text" onChange={handleChange}></input>
         </div>
       </div>
       <div classname='field'>
         <label className='label' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Verify Password</label>
         <div className='control' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='email' ></input>
+          <input className='input is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '30%' }} type='email' onChange={handleChange}></input>
         </div>
         <p className='help is-danger' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>This Password is invalid!</p>
       </div>
@@ -47,7 +82,7 @@ function Signup() {
         <label className='label' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Choose county!</label>
         <div className='control' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <div className='select is-small' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <select style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <select style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} onChange={handleChange}>
               <option>Atlantic</option>
               <option>Bergen</option>
               <option>Burlington</option>
@@ -76,15 +111,15 @@ function Signup() {
       <div className='field'>
         <div className='control'>
           <label className='checkbox' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <input type='checkbox' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}></input>
+            <input type='checkbox' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} onChange={handleChange}></input>
             I agree to the <a href='#terms'>terms and conditions.</a>
           </label>
         </div>
       </div>
       <div className='buttons' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <button className='button is-info is-light is-small'>Sign-Up</button>
+        <button type='submit' className='button is-info is-light is-small'>Sign-Up</button>
       </div>
-    </>
+    </form>
 
   )
 };
